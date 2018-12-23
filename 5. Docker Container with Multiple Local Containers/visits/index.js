3d12a991db94cc6ffbaedc196ce8fc9ca14947cb
Chandan Kumar <chandanchaudhary@outlook.com>
2018-12-23 22:57:35 +0530

    Add server code of the visits app

---
+++ 5. Docker Container with Multiple Local Containers/visits/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient();
+
+const key = 'visits';
+
+client.set(key, 0);
+
+app.get('/', (req, res) => {
+  client.get(key, (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set(key, +visits + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log('Listening to port 8081');
+});
